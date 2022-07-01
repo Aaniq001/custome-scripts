7149a07d3dce5565df24c4bbaa53969d43c10f14
CareCart Shopify APP <44872750+carecartapp@users.noreply.github.com>
2022-07-01 11:36:00 +0500

    CTI-586-3 custom-location-visitor.js

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -606,7 +606,7 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
 
     if (Shopify.shop == "medici-supply-co.myshopify.com")
     {
-        customSelctor = $jq321("#AddToCart-7209334571170");
+        customSelctor = $jq321(".add-to-cart");
         finalSelector = customSelctor[0];
 
         console.log(customSelctor);
