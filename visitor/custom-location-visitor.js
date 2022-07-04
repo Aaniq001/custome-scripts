f38e361df183216ed082d9a68dd8827ed2650163
CareCart Shopify APP <44872750+carecartapp@users.noreply.github.com>
2022-07-04 13:33:23 +0500

    CTI-601 custom-location-visitor.js

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -312,6 +312,12 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
 
         console.log((meta.product && meta.product.id)?meta.product.id:'');
     }
+    if (Shopify.shop == "amamibijoux.myshopify.com") 
+    {
+        var meta = {"product":{"id":__st.rid}};
+
+        console.log((meta.product && meta.product.id)?meta.product.id:'');
+    }
 
 
     $jq321.ajax({
@@ -621,6 +627,14 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     if (Shopify.shop == "wrywryshop.myshopify.com")
     {
         customSelctor = $jq321(".ProductForm__Variants");
+        finalSelector = customSelctor[0];
+
+        console.log(customSelctor);
+    }
+
+    if (Shopify.shop == "amamibijoux.myshopify.com")
+    {
+        customSelctor = $jq321(".product__title");
         finalSelector = customSelctor[0];
 
         console.log(customSelctor);
