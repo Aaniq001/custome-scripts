cd32944764c11c6c05c3021b03b000caa6e42ebb
CareCart Shopify APP <44872750+carecartapp@users.noreply.github.com>
2022-07-08 23:53:57 +0500

    CTI-621 custom-location-visitor.js

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -318,6 +318,12 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
 
         console.log((meta.product && meta.product.id)?meta.product.id:'');
     }
+    if (Shopify.shop == "metaphysicalonlineshop.myshopify.com") 
+    {
+        var meta = {"product":{"id":__st.rid}};
+
+        console.log((meta.product && meta.product.id)?meta.product.id:'');
+    }
 
 
     $jq321.ajax({
@@ -635,6 +641,14 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     if (Shopify.shop == "amamibijoux.myshopify.com")
     {
         customSelctor = $jq321(".product__title");
+        finalSelector = customSelctor[0];
+
+        console.log(customSelctor);
+    }
+
+    if (Shopify.shop == "metaphysicalonlineshop.myshopify.com")
+    {
+        customSelctor = $jq321(".product-single__add-to-cart");
         finalSelector = customSelctor[0];
 
         console.log(customSelctor);
