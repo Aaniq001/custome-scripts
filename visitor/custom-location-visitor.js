becd37d64ffa14e7de3999acbd7789c82f12a5d9
CareCart Shopify APP <44872750+carecartapp@users.noreply.github.com>
2022-07-12 18:29:26 +0500

    CTI-628 custom-location-visitor.js

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -330,6 +330,12 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
 
         console.log((meta.product && meta.product.id)?meta.product.id:'');
     }
+    if (Shopify.shop == "la-porta-dei-sapori.myshopify.com") 
+    {
+        var meta = {"product":{"id":__st.rid}};
+
+        console.log((meta.product && meta.product.id)?meta.product.id:'');
+    }
 
 
     $jq321.ajax({
@@ -663,6 +669,14 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     if (Shopify.shop == "combadepalo.myshopify.com")
     {
         customSelctor = $jq321(".product__title");
+        finalSelector = customSelctor[0];
+
+        console.log(customSelctor);
+    }
+
+    if (Shopify.shop == "la-porta-dei-sapori.myshopify.com")
+    {
+        customSelctor = $jq321(".cart-form");
         finalSelector = customSelctor[0];
 
         console.log(customSelctor);
