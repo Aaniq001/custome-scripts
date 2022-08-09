9ef71abc2ba5b0e4a0fe4907cdd733c617a48ea7
CareCart Shopify APP <44872750+carecartapp@users.noreply.github.com>
2022-08-09 22:22:01 +0500

    CTI-701 custom-location-visitor.js

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -348,6 +348,12 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
 
         console.log((meta.product && meta.product.id)?meta.product.id:'');
     }
+    if (Shopify.shop == "vitapur-stage-store.myshopify.com") 
+    {
+        var meta = {"product":{"id":__st.rid}};
+
+        console.log((meta.product && meta.product.id)?meta.product.id:'');
+    }
 
 
     $jq321.ajax({
@@ -705,6 +711,20 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     if (Shopify.shop == "ohmepretty.myshopify.com")
     {
         customSelctor = $jq321("#AddToCart--template--15150626898052__main");
+        finalSelector = customSelctor[0];
+
+        console.log(customSelctor);
+    }
+
+    if (Shopify.shop == "vitapur-stage-store.myshopify.com")
+    {
+        $jq321("head").append(
+            '<style type="text/css">' + 
+            '.visitor-counter-content-box-carecartbysalespop-2020{ height: auto !important; margin-top: 0 !important; }' +
+            '</style>'
+           );
+
+        customSelctor = $jq321(".product__title");
         finalSelector = customSelctor[0];
 
         console.log(customSelctor);
