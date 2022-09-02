65b09e5d8164d689b449e9f265c8c11e79817ced
CareCart Shopify APP <44872750+carecartapp@users.noreply.github.com>
2022-09-02 16:30:55 +0500

    CTI-803 customization-salesnotifier.js

--- sales-pop/customization-salesnotifier.js
+++ sales-pop/customization-salesnotifier.js
@@ -1250,6 +1250,25 @@
             }
         }
 
+        if (Shopify.shop == "global-gochujar.myshopify.com") 
+        {
+            if (block_url[2] == 'login')
+            {
+                b_url = false;
+                console.log('SP Sales Notification Block on "Login" page'); 
+            }
+            else if (block_url[2] == 'register')
+            {
+                b_url = false;
+                console.log('SP Sales Notification Block on "Register" page'); 
+            }
+            else if (block_url[1] == 'challenge')
+            {
+                b_url = false;
+                console.log('SP Sales Notification Block on "challenge" page'); 
+            }
+        }
+
         return b_url;
 
      };
