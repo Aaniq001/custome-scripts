4970040926e3efe375e17fdf3f5a339bc0583027
CareCart Shopify APP <44872750+carecartapp@users.noreply.github.com>
2022-10-03 11:05:43 +0500

    CTI-919 custom-location-visitor.js

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -407,6 +407,12 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
 
         //console.log((meta.product && meta.product.id)?meta.product.id:'');
     }
+    if (Shopify.shop == "protectich.myshopify.com") 
+    {
+        var meta = {"product":{"id":__st.rid}};
+
+        //console.log((meta.product && meta.product.id)?meta.product.id:'');
+    }
 
     $jq321.ajax({
         type: "GET",
@@ -850,6 +856,14 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     if (Shopify.shop == "nomardic-com.myshopify.com")
     {
         customSelctor = $jq321(".ProductForm__AddToCart");
+        finalSelector = customSelctor[0];
+
+        console.log(customSelctor);
+    }
+
+    if (Shopify.shop == "protectich.myshopify.com")
+    {
+        customSelctor = $jq321(".product__tax");
         finalSelector = customSelctor[0];
 
         console.log(customSelctor);
