f8cdbb215958a72f45704a3e0cdd47fce16ea881
CareCart Shopify APP <44872750+carecartapp@users.noreply.github.com>
2022-10-11 11:44:46 +0500

    CTI-943 custom-location-visitor.js

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -413,6 +413,12 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
 
         //console.log((meta.product && meta.product.id)?meta.product.id:'');
     }
+    if (Shopify.shop == "mirable-life.myshopify.com") 
+    {
+        var meta = {"product":{"id":__st.rid}};
+
+        //console.log((meta.product && meta.product.id)?meta.product.id:'');
+    }
 
     $jq321.ajax({
         type: "GET",
@@ -864,6 +870,14 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     if (Shopify.shop == "protectich.myshopify.com")
     {
         customSelctor = $jq321(".product__tax");
+        finalSelector = customSelctor[0];
+
+        console.log(customSelctor);
+    }
+
+    if (Shopify.shop == "mirable-life.myshopify.com")
+    {
+        customSelctor = $jq321(".pf-360_");
         finalSelector = customSelctor[0];
 
         console.log(customSelctor);
