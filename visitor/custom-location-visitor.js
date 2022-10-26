342fb7dd40bc487cab2fcdef538661464ac6f6a2
CareCart Shopify APP <44872750+carecartapp@users.noreply.github.com>
2022-10-26 11:10:21 +0500

    CTI-988 custom-location-visitor.js

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -449,6 +449,12 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
 
         //console.log((meta.product && meta.product.id)?meta.product.id:'');
     }
+    if (Shopify.shop == "sancta-maria-epl.myshopify.com") 
+    {
+        var meta = {"product":{"id":__st.rid}};
+
+        //console.log((meta.product && meta.product.id)?meta.product.id:'');
+    }
 
     $jq321.ajax({
         type: "GET",
@@ -948,6 +954,21 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     if (Shopify.shop == "carboneclothes2.myshopify.com")
     {
         customSelctor = $jq321(".pf-37_");
+        finalSelector = customSelctor[0];
+
+        console.log(customSelctor);
+    }
+
+    if (Shopify.shop == "sancta-maria-epl.myshopify.com")
+    {
+        $jq321("head").append(
+            '<style type="text/css">' + 
+            '.visitor-counter-content-box-carecartbysalespop-2020 {display: block !important;}' +
+            '#effectiveAppsProductVisitorsCounterElement, .visitor-counter-content-box-carecartbysalespop-2020, .visitor-left {display: block!important; }' +
+            '</style>'
+           );
+
+        customSelctor = $jq321(".prd-block_actions");
         finalSelector = customSelctor[0];
 
         console.log(customSelctor);
