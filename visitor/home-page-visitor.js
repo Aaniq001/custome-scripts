87635a42629b610b7564056470e5a1e12f8cf6ca
CareCart Shopify APP <44872750+carecartapp@users.noreply.github.com>
2022-11-21 16:20:00 +0500

    CTI-1128-2 home-page-visitor.js

--- visitor/home-page-visitor.js
+++ visitor/home-page-visitor.js
@@ -209,7 +209,7 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     }
     if(Shopify.shop == "gohoa.myshopify.com")
     {
-        customSelctor = $jq321(".footer__content-bottom-wrapper").parent();
+        customSelctor = $jq321(".footer__content-bottom-wrapper");
         finalSelector = customSelctor[0];
 
         console.log(customSelctor);
