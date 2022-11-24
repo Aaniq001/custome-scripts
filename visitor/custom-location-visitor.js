09a726fd84f7d9f15f6dad9c780226b685449d81
CareCart Shopify APP <44872750+carecartapp@users.noreply.github.com>
2022-11-24 17:58:51 +0500

    CTI-1149 custom-location-visitor.js

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -491,6 +491,12 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
 
         //console.log((meta.product && meta.product.id)?meta.product.id:'');
     }
+    if (Shopify.shop == "cengiz-akturk-shop.myshopify.com") 
+    {
+        var meta = {"product":{"id":__st.rid}};
+
+        //console.log((meta.product && meta.product.id)?meta.product.id:'');
+    }
 
     $jq321.ajax({
         type: "GET",
@@ -1062,6 +1068,14 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     if (Shopify.shop == "buyur-ltd.myshopify.com")
     {
         customSelctor = $jq321(".product-form__payment-container");
+        finalSelector = customSelctor[0];
+
+        console.log(customSelctor);
+    }
+
+    if (Shopify.shop == "cengiz-akturk-shop.myshopify.com")
+    {
+        customSelctor = $jq321(".product__submit__buttons");
         finalSelector = customSelctor[0];
 
         console.log(customSelctor);
