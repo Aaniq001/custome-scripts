12c128c9657073461dba0a0f048f2bf5c08231c3
CareCart Shopify APP <44872750+carecartapp@users.noreply.github.com>
2022-12-05 14:37:03 +0500

    CTI-1193 custom-location-visitor.js

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -509,6 +509,12 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
 
         //console.log((meta.product && meta.product.id)?meta.product.id:'');
     }
+    if (Shopify.shop == "www-minkymyles-com.myshopify.com") 
+    {
+        var meta = {"product":{"id":__st.rid}};
+
+        //console.log((meta.product && meta.product.id)?meta.product.id:'');
+    }
 
     $jq321.ajax({
         type: "GET",
@@ -1104,6 +1110,14 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     if (Shopify.shop == "osim-germany.myshopify.com")
     {
         customSelctor = $jq321(".flex");
+        finalSelector = customSelctor[0];
+
+        console.log(customSelctor);
+    }
+
+    if (Shopify.shop == "www-minkymyles-com.myshopify.com")
+    {
+        customSelctor = $jq321(".rte");
         finalSelector = customSelctor[0];
 
         console.log(customSelctor);
