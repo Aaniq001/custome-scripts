8df1a8b5e631ed35a512dff3e4f7ce2b5a1abe55
CareCart Shopify APP <44872750+carecartapp@users.noreply.github.com>
2022-12-12 11:14:20 +0500

    CTI-1215 custom-location-visitor.js

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -520,6 +520,12 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
 
         //console.log((meta.product && meta.product.id)?meta.product.id:'');
     }
+    if (Shopify.shop == "simplemerit-store.myshopify.com") 
+    {
+        var meta = {"product":{"id":__st.rid}};
+
+        //console.log((meta.product && meta.product.id)?meta.product.id:'');
+    }
 
     $jq321.ajax({
         type: "GET",
@@ -1123,6 +1129,14 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     if (Shopify.shop == "www-minkymyles-com.myshopify.com")
     {
         customSelctor = $jq321(".product-form__buttons");
+        finalSelector = customSelctor[0];
+
+        console.log(customSelctor);
+    }
+
+    if (Shopify.shop == "simplemerit-store.myshopify.com")
+    {
+        customSelctor = $jq321(".product-single__add-to-cart");
         finalSelector = customSelctor[0];
 
         console.log(customSelctor);
