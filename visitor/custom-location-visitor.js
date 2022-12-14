e8d412b02807d23d535daec57cdb520c75c258b5
CareCart Shopify APP <44872750+carecartapp@users.noreply.github.com>
2022-12-14 11:56:51 +0500

    CTI-1223 custom-location-visitor.js

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -526,6 +526,12 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
 
         //console.log((meta.product && meta.product.id)?meta.product.id:'');
     }
+    if (Shopify.shop == "monnarc.myshopify.com") 
+    {
+        var meta = {"product":{"id":__st.rid}};
+
+        //console.log((meta.product && meta.product.id)?meta.product.id:'');
+    }
 
     $jq321.ajax({
         type: "GET",
@@ -1137,6 +1143,14 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     if (Shopify.shop == "simplemerit-store.myshopify.com")
     {
         customSelctor = $jq321(".product-single__add-to-cart");
+        finalSelector = customSelctor[0];
+
+        console.log(customSelctor);
+    }
+
+    if (Shopify.shop == "monnarc.myshopify.com")
+    {
+        customSelctor = $jq321(".payment-and-quantity");
         finalSelector = customSelctor[0];
 
         console.log(customSelctor);
